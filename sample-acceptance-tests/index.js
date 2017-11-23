5b880425eda6245b836d1bb03ebbbb25487ff91d
Michael Warren <michaelawarren@outlook.com>
2017-11-23 16:35:27 -0700

    getting things to work

--- sample-acceptance-tests/index.js
+++ sample-acceptance-tests/index.js
@@ -1,8 +1,9 @@
 var http = require('http');
 
  http.createServer(function (request, response) {
-     response.writeHead(200, {'Content-Type': 'text/plain'});
-     response.end('Hello World\n');
+     response.writeHead(200, {'Content-Type': 'application/json'});
+     response.end(JSON.stringify({'message': 'Hello World!'}));
+     console.log('end hit')
  }).listen(8080);
 
  console.log('Server started');
--- sample-acceptance-tests/spec/myFirstAcceptanceTest.accept.spec.js
+++ sample-acceptance-tests/spec/myFirstAcceptanceTest.accept.spec.js
@@ -1,10 +1,10 @@
 describe ("request to root", function(){
-    it ("should return Hello World", function() {
+    it ("should return Hello World", function(done) {
         var request = require("request");
-        var message = "fail";
-        request.get("http://localhost:8080")., function (error, response, body){
-            message = body;
+
+        request("http://localhost:8080", function (error, response, body){
+            expect(body).toBe(JSON.stringify({'message': 'Hello World!'}));
+            done();
         });
-        console.log(message)
     });
 });
